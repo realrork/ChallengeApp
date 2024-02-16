1f5d7b9da2b3af60e087d0420a335c13227e8a36
nockeer <rork@outlook.com>
2024-02-16 23:50:31 +0100

    Zmiany zrobione w zadaniu domowym dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,6 +2,21 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Dominik");
+        string name = "Dominik";
+        int age = 17;
+        char sex = 'M';
+
+        if (age < 30 && sex == 'K')
+        {
+            Console.WriteLine("Kobieta poniżej 30 lat");
+        }
+        else if (name == "Ewa" && age == 30)
+        {
+            Console.WriteLine("Ewa. lat 30");
+        }
+        else if (age < 18 && sex == 'M')
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+        }
     }
-} 
+}
